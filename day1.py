079c04111f2c0ad54bf9804d682ae9ca38305963
Erza-007 <semwalanjali007@gmail.com>
2026-05-13 09:46:47 +0530

    add day1 programs

---
+++ day1.py
@@ -0,0 +1,10 @@
+#n=input("enter your name:")
+#print("helloooo",n)
+#
+#n=int(input("enter a number to check:"))
+#
+#l1=["apple","strawberry","kiwi","mango"]
+#rint(l1)
+n=int(input("enter a number to add:"))
+a=int(input("enter another number to add:"))
+print(n+a)
