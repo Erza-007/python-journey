a5c7b3a6b2ba398e0fc5b7cc4150f9d23c28d42c
Erza-007 <semwalanjali007@gmail.com>
2026-05-13 14:47:17 +0530

    add day1 programs

--- day1.py
+++ day1.py
@@ -5,6 +5,23 @@
 #
 #l1=["apple","strawberry","kiwi","mango"]
 #rint(l1)
-n=int(input("enter a number to add:"))
-a=int(input("enter another number to add:"))
-print(n+a)
+#n=int(input("enter a number to add:"))
+#a=int(input("enter another number to add:"))
+#print(n+a)
+message = "hello anjali"
+print(message.upper())
+V=["a","e","i","o","u","A","E","I","O","U"]
+a=0
+for i in range(len(message)):
+    if message[i] in V:
+        a=a+1
+    else:        pass
+print(a)
+print(message[::-1])
+same="madam"
+if same==same[::-1]:
+    print("palindrome")
+else:
+    print("not palindrome")
+print(message.replace(" ","_"))
+    
